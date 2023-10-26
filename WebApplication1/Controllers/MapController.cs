d34b2d06336b34dd2cd96b70764dbcb09f5db875
FPZ\terdelic <terdelic@fpz.hr>
2023-10-26 14:30:42 +0200

    Add project files.

---
+++ WebApplication1/Controllers/MapController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace WebApplication1.Controllers
+{
+    public class MapController : Controller
+    {
+        // GET: Map
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // public ActionResult ComputeConture(double x, double y, string type, double trTime, double trEner, string typeCountreMethod)
+        [HttpPost]
+        public ActionResult ComputeConture(string longitude, string latitude)
+        {
+            Console.WriteLine();
+
+            return Content(JsonConvert.SerializeObject("Uspjesno"), "application/json"); ;
+        }
+    }
+}
